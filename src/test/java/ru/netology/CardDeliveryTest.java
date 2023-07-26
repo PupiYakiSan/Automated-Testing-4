ae0b668d8f18344858d63f305f822d58a318fbf8
Olga <o.n.p@mail.ru>
2023-07-26 21:33:39 +0300

    Add tests task2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
@@ -207,6 +209,53 @@ public class CardDeliveryTest {
 
         $("[data-test-id='agreement'].input_invalid .checkbox__text").shouldBe(Condition.visible);
 
+    }
+
+    @Test
+    void successSubmitFormCitySelectList() {
+
+        $("[data-test-id=city] input").val("Ка");
+
+        $(By.className("menu")).shouldBe(Condition.visible);
+        $$(By.className("menu-item__control")).find(Condition.text("Сыктывкар")).click();
+
+        $("[data-test-id=name] input").val("Петров Иван");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15));
+    }
+
+    @Test
+    void cityNoList() {
+
+        $("[data-test-id=city] input").val("Лю");
+        $(By.className("menu")).shouldNotBe(Condition.visible);
+    }
+
+    @Test
+    void successSubmitFormCalendarDate() {
+
+        $("[data-test-id=city] input").val("Москва");
+
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, 7); // увеличиваем на 7 дней от текущей даты
+
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+        String str1 = format1.format(c.getTime());
+
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd"); //придаем нужный формат дате
+        String str2 = format2.format(c.getTime());
+
+        $(By.className("calendar-input")).click();
+
+        $("[data-test-id=name] input").val("Петров Иван");
+        $("[data-test-id=phone] input").val("+79001112233");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15));
     }
 
 }
